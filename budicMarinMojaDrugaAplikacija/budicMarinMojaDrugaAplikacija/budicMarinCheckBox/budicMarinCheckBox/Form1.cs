27d33941bd73144eb5a0193eef8c29c24f7d3735
budicmarin <55134480+budicmarin@users.noreply.github.com>
2019-09-24 15:42:08 +0200

    Add files via upload

---
+++ budicMarinMojaDrugaAplikacija/budicMarinMojaDrugaAplikacija/budicMarinCheckBox/budicMarinCheckBox/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace budicMarinCheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void CheckBoxDorucak_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxOdabrali.Text ="Dorucak";
+        }
+
+        private void CheckBoxRucak_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBoxVecera_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckSakriNarudzbu_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPosalji_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxOdabraliSte_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
