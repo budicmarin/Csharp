9f793fdf4cd6a1d10a7154d8ff7349ae9ebc4a3e
budicmarin <55134480+budicmarin@users.noreply.github.com>
2019-09-24 12:12:13 +0200

    Add files via upload

---
+++ budicMarinMojaDrugaAplikacija/budicMarinMojaDrugaAplikacija/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace budicMarinMojaDrugaAplikacija
+{
+    public partial class Form1 : Form
+    {
+        int broj = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            broj++;
+            txtPrikaz.Text = broj+"";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            broj--;
+            txtPrikaz.Text = broj.ToString();
+        }
+
+        private void TxtPrikaz_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
